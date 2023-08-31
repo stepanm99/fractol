f2753db62c1fd102451fc93817026af3133a473b
smelicha <smelicha@student.42heilbronn.de>
2023-08-31 16:29:44 +0200

    I am trying to fill image with red color and put it into the window but it seems it doesn't work for some reason...

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 16:06:41 by smelicha          #+#    #+#             */
-/*   Updated: 2023/08/31 16:05:13 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/08/31 16:28:44 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,27 @@
 int	main()
 {
 	void	*mlx_ptr;
-	void	*mlx_window;
+	void	*mlx_ip;
+	int		ix;
+	int		iy;
 
-
+	ix = 0;
+	iy = 0;
 	mlx_ptr = mlx_init(640, 480, "Test", true);
+	mlx_ip = mlx_new_image(mlx_ptr, 640, 480);
+	while (iy != 480)
+	{
+		printf("%i\n", iy);
+		while (ix != 640)
+		{
+			mlx_put_pixel(mlx_ip, ix, iy, 0x00ff0000);
+			ix++;
+		}
+		ix = 0;
+		iy++;
+	}
+	mlx_put_pixel(mlx_ip, 320, 240, 0x00ff0000);
+	mlx_image_to_window(mlx_ptr, mlx_ip, 640, 480);
 	mlx_loop(mlx_ptr);
 	printf("MLX pointer: %p", mlx_ptr);
 	sleep(3);
